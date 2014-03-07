08f23c328ff595ea20a73a2f1272c2967ca34b55
Charles Ninane <charles.ninane@student.uclouvain.be>
2014-03-07 16:10:32 +0100

    comment créer plusieurs activités

---
+++ Test multifenetre/src/com/example/whattowatch/ThirthActivity.java
@@ -0,0 +1,14 @@
+package com.example.whattowatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThirthActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
