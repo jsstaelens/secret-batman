4d797061d8f6180bade0530b8423751bd964d3fd
QuentinG3 <QG3393@hotmail.com>
2014-09-27 21:26:22 +0200

    stuff

--- src/main.java
+++ src/main.java
@@ -1,13 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class main {
 
 	public static void main(String[] args) {
 		ArrayList<String> stringList = new ArrayList<String>();
-		stringList = reader("source.txt");
+		stringList = reader("input.txt");
+		resetOutputFile("ouput.txt");
 		Stack stack = new Stack();
 		for(int i = 0;i<stringList.size();i++)
 		{
@@ -75,11 +78,60 @@ public class main {
 				if(stringList.get(i).equals("pstack"))
 				{
 					Stack stack2 = new Stack();
+					while(!stack.isEmpty())
+					{
+						String str = stack.pop();
+						System.out.println(str);
+						writter(str,"output.txt");
+						stack2.push(str);
+					}
+					while(!stack2.isEmpty())
+					{
+						stack.push(stack2.pop());
+					}
 				}
+				if(stringList.get(i).equals("dup"))
+				{
+					String str = stack.pop();
+					stack.push(str);
+					stack.push(str);
+				}
+				if(stringList.get(i).equals("exch"))
+				{
+					String str1 = stack.pop();
+					String str2 = stack.pop();
+					stack.push(str1);
+					stack.push(str2);
+				}
+				if(stringList.get(i).equals("eq"))
+				{
+					String str1 = stack.pop();
+					String str2 = stack.pop();
+					if(Double.parseDouble(str1) == Double.parseDouble(str2)){
+						stack.push("true");
+					}
+					else
+					{
+						stack.push("false");
+					}
+				}
+				if(stringList.get(i).equals("ne"))
+				{
+					String str1 = stack.pop();
+					String str2 = stack.pop();
+					if(Double.parseDouble(str1) == Double.parseDouble(str2)){
+						stack.push("false");
+					}
+					else
+					{
+						stack.push("true");
+					}
+				}
+				
 				
 			}
 		}
-		System.out.println(stack.pop());
+		
 		
 	
 		
@@ -87,7 +139,40 @@ public class main {
 		
 		
 	}
-
+	public static void resetOutputFile(String destination)
+	{
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		try{
+			fw = new FileWriter("output.txt",false);
+	        pw = new PrintWriter(fw);
+		pw.close();
+		fw.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Probleme lors de l'écriture");
+		}
+	}
+	public static void writter(String texte,String destination)
+	{
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		try{
+			fw = new FileWriter("output.txt",true);
+	        pw = new PrintWriter(fw);
+		
+		
+		
+		pw.println(texte);
+		pw.close();
+		fw.close();
+	}
+	catch(IOException e)
+	{
+		System.out.println("Probleme lors de l'écriture");
+	}
+	}
 	public static ArrayList<String> reader(String source) {
 		BufferedReader br = null;
 		ArrayList<String> stringList = new ArrayList<String>();
