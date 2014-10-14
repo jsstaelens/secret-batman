e79b984198e4e25e9ae166dfca0c21c6a69cb13f
wenaud <wenders_audrey@hotmail.com>
2014-10-14 09:07:40 +0200

    création du projet de base

---
+++ m2/src/mission2/FormalExpressionTreeImplemented.java
@@ -0,0 +1,20 @@
+package mission2;
+
+/**
+ *
+ * @author Groupe 5.2
+ */
+public class FormalExpressionTreeImplemented implements FormalExpressionTree {
+
+    @Override
+    public FormalExpressionTree derive() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public String toString(){
+        return null;    
+    }
+    
+    
+}
---
+++ m2/src/mission2/Main.java
@@ -0,0 +1,15 @@
+package mission2;
+
+/**
+ *
+ * @author Groupe 5.2
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
