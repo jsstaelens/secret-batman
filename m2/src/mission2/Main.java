8fa4b6f0a445754d4b8cf7264aca7f4efb2e0878
obronchain <olivier.bronchain@student.uclouvain.be>
2014-10-16 17:15:39 +0200

    can read file

--- m2/src/mission2/Main.java
+++ m2/src/mission2/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -10,7 +12,12 @@
 public class Main {
     
     public static void main(String[] args) {
-        FormalExpressionTreeImplemented arbre = new FormalExpressionTreeImplemented("3+((x^2)-9)");
+    	FileIO fileIn = new FileIO("input");
+    	ArrayList<String> ar = fileIn.readByLine();
+    	for(int i=0 ; i< ar.size() ; i++){
+    		FormalExpressionTreeImplemented arbre = new FormalExpressionTreeImplemented(ar.get(i));
+    	}
     }
+    	
     
 }
